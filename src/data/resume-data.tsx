a709c1cd986188a5017601f2595bc5b3559dff0c
Nuno Guterres Nogueira <guterresnogueira@gmail.com>
2024-03-15 20:56:51 +0100

    .

--- src/data/resume-data.tsx
+++ src/data/resume-data.tsx
@@ -20,259 +20,150 @@ import {
 import { GitHubIcon, LinkedInIcon, XIcon } from "@/components/icons";
 
 export const RESUME_DATA = {
-  name: "Bartosz Jarocki",
-  initials: "BJ",
-  location: "Wrocław, Poland, CET",
-  locationLink: "https://www.google.com/maps/place/Wrocław",
+  name: "Nuno Nogueira",
+  initials: "NN",
+  location: "Stockholm, Sweden, CET",
   about:
-    "Full Stack Engineer focused on building products with extra attention to detail",
-  summary:
-    "As a Full Stack Engineer, I have successfully taken multiple products from 0 to 1. I lead teams effectively, ensuring an environment where people can do their best work. Currently, I work mostly with TypeScript, React, Node.js, and GraphQL. I have over 8 years of experience in working remotely with companies all around the world.",
-  avatarUrl: "https://avatars.githubusercontent.com/u/1017620?v=4",
-  personalWebsiteUrl: "https://jarocki.me",
+    "Embedded Software Developer focused on making things.",
+  summary: "As an Embedded Software Developer I've acquired experience with " +
+    "microcontrollers, Linux and real time operating systems with a strong link between " +
+    "the device and the cloud. Mostly focused on C/C++, CMake and Javascript. I successfully build, test and maintain code for " +
+    "systems on the field. My hobbyist side also works with PCB & CAD Design.",
+  avatarUrl: "https://pbs.twimg.com/profile_images/1751600040450125824/MjiaUKKF_400x400.jpg",
+  personalWebsiteUrl: "https://nguterresn.github.io/quartz",
   contact: {
-    email: "bartosz.jarocki@hey.com",
-    tel: "+48530213401",
+    email: "guterresnogueira@gmail.com",
+    tel: "++460793511070",
     social: [
       {
         name: "GitHub",
-        url: "https://github.com/BartoszJarocki",
+        url: "https://github.com/nguterresn",
         icon: GitHubIcon,
       },
       {
         name: "LinkedIn",
-        url: "https://www.linkedin.com/in/bjarocki/",
+        url: "https://www.linkedin.com/in/guterresnogueira/",
         icon: LinkedInIcon,
       },
       {
         name: "X",
-        url: "https://x.com/BartoszJarocki",
+        url: "https://twitter.com/nunoguterres",
         icon: XIcon,
       },
     ],
   },
   education: [
     {
-      school: "Wrocław University of Technology",
-      degree: "Bachelor's Degree in Control systems engineering and Robotics",
-      start: "2007",
-      end: "2010",
+      school: "University of Porto",
+      degree: "Master's degree in Electrical & Computer Engineering with " +
+        "specialization in Embedded Systems and Microelectronics.",
+      start: "2018",
+      end: "2021",
     },
+    {
+      school: "Polytechnic University of Porto",
+      degree: "Bachelor's Degree in Electrical & Computer Engineering",
+      start: "2015",
+      end: "2018",
+    }
   ],
   work: [
     {
-      company: "Parabol",
-      link: "https://parabol.co",
-      badges: ["Remote"],
-      title: "Senior Full Stack Developer",
-      logo: ParabolLogo,
+      company: "Kisi",
+      link: "https://www.getkisi.com",
+      badges: [],
+      title: "IoT Developer",
       start: "2021",
-      end: "2024",
+      end: "Current",
       description:
-        "Implemented new features, led squad, worked on improving the way developers ship the code, started migration from Emotion to Tailwind CSS and more. Technologies: React, TypeScript, GraphQL",
+        "In charge of building, testing, and maintaining all Kisi devices. Guided the team through " +
+        "the advancement of the next generation of access control devices and helped improve the " +
+        "team development environment and tools."
     },
     {
-      company: "Clevertech",
-      link: "https://clevertech.biz",
-      badges: ["Remote"],
-      title: "Lead Android Developer → Full Stack Developer",
-      logo: ClevertechLogo,
-      start: "2015",
+      company: "Academia de Robotica",
+      link: "https://www.academiarobotica.pt",
+      badges: [],
+      title: "Robotics Teacher",
+      start: "2020",
       end: "2021",
       description:
-        "Created Android mobile apps and led teams for companies like Vision Media, DKMS, or AAA. Built live streaming application for Evercast from scratch. Technologies: Android, Kotlin, React, TypeScript, GraphQL",
+        "Instructed and guided children and adolescents as they embarked on their initial" +
+        "exploration of the world of robotics.",
     },
     {
-      company: "Jojo Mobile",
-      link: "https://bsgroup.eu/",
+      company: "AddVolt",
+      link: "https://addvolt.com",
       badges: [],
-      title: "Android Developer → Lead Android Developer",
-      logo: JojoMobileLogo,
-      start: "2012",
-      end: "2015",
+      title: "Intern",
+      start: "2018",
+      end: "2018",
       description:
-        "Built an Android team, created Android apps for biggest Polish companies like LOT, Polskie Radio, Agora, PolskaPress",
-    },
-    {
-      company: "Nokia Siemens Networks",
-      link: "https://www.nokia.com",
-      badges: [],
-      title: "C/C++ Developer",
-      logo: NSNLogo,
-      start: "2010",
-      end: "2012",
-      description: "Creating and testing software for LTE base stations",
+        "Deployed an over-the-air anti-faults protocol to a multi-node network.",
     },
   ],
   skills: [
-    "JavaScript",
-    "TypeScript",
-    "React/Next.js/Remix",
-    "Node.js",
-    "GraphQL",
-    "Relay",
-    "WebRTC",
+    "C",
+    "C++",
+    "IoT",
+    "RTOS",
+    "ARM32",
+    "AVR",
+    "Embedded Systems",
+    "Linux",
+    "Control Systems",
+    "Python",
+    "Javascript",
+    "Google Cloud Platform",
+    "PCB Design (Kicad)",
+    "CAD Design (Fusion360)",
   ],
   projects: [
     {
-      title: "Consultly",
+      title: "CMake Gecko SDK",
       techStack: [
         "Side Project",
-        "TypeScript",
-        "Next.js",
-        "Vite",
-        "GraphQL",
-        "WebRTC",
+        "CMake",
       ],
-      description: "A platform to build and grow your online business",
-      logo: ConsultlyLogo,
+      description: "Gecko SDK Blinky example using CMake.",
       link: {
-        label: "consultly.com",
         href: "https://consultly.com/",
       },
     },
     {
-      title: "Monito",
-      techStack: ["Side Project", "TypeScript", "Next.js", "Browser Extension"],
+      title: "BattleButBot (WIP)",
+      techStack: ["Side Project", "ESP32", "CAD Design", "PCB Design", "C/C++"],
       description:
-        "Browser extension that records everything happening in a web application",
-      logo: MonitoLogo,
+        "Your Ultimate Antweight Fighting Robot Companion",
       link: {
-        label: "monito.dev",
-        href: "https://monito.dev/",
-      },
-    },
-    {
-      title: "Jarocki.me",
-      techStack: ["Side Project", "Next.js", "MDX"],
-      description:
-        "My personal website and blog. Built with Next.js and Notion API",
-      logo: JarockiMeLogo,
-      link: {
-        label: "github.com",
         href: "https://jarocki.me/",
       },
     },
     {
-      title: "Minimal",
-      techStack: ["Side Project", "Next.js", "Puppeteer"],
+      title: "Portable Smoke Extractor",
+      techStack: ["Side Project", "CAD Design", "PCB Design"],
       description:
-        "Minimalist calendars, habit trackers and planners generator",
-      logo: Minimal,
+        "A portable Smoke Extractor to deal with all those nasty fumes!",
       link: {
-        label: "useminimal.com",
-        href: "https://useminimal.com/",
+        href: "https://github.com/nguterresn/smoke-extractor",
       },
     },
     {
-      title: "Barepapers",
-      techStack: ["Side Project", "Next.js", "Puppeteer"],
+      title: "End Of Line Testing Board",
+      techStack: ["Side Project", "CAD Design", "PCB Design", "Access Control"],
       description:
-        "Generates beautiful wallpapers using random shapes and gradients",
-      logo: BarepapersLogo,
+        "A End Of Line Board for Testing Access Control Systems",
       link: {
-        label: "barepapers.com",
-        href: "https://barepapers.com/",
+        href: "https://github.com/nguterresn/end-of-line-board",
       },
     },
     {
-      title: "Year progress",
-      techStack: ["Side Project", "TypeScript", "Next.js"],
-      description: "Tracks current year progress and displays a countdown",
-      logo: YearProgressLogo,
+      title: "Arduino Due + KA3005P",
+      techStack: ["Side Project", "Arduino", "USB"],
+      description: "Library to facilitate seamless communication between the Due and the KA3005P power supply",
       link: {
-        label: "getyearprogress.com",
-        href: "https://getyearprogress.com/",
+        href: "https://github.com/nguterresn/due-ka3005p",
       },
-    },
-    {
-      title: "Parabol",
-      techStack: [
-        "Full Stack Developer",
-        "TypeScript",
-        "React",
-        "Node.js",
-        "GraphQL",
-      ],
-      description:
-        "The Agile meeting co-pilot that delivers better meetings with less effort",
-      logo: ParabolLogo,
-      link: {
-        label: "github.com",
-        href: "https://parabol.co/",
-      },
-    },
-    {
-      title: "Evercast",
-      techStack: [
-        "Lead Frontend Developer",
-
-        "TypeScript",
-        "React",
-        "Node.js",
-        "GraphQL",
-      ],
-      description:
-        "Creative collaboration platform that combines video conferencing and HD media streaming",
-      logo: EvercastLogo,
-      link: {
-        label: "evercast.us",
-        href: "https://www.evercast.us/",
-      },
-    },
-    {
-      title: "Mobile Vikings",
-      techStack: ["Lead Android Developer", "Android", "Kotlin"],
-      description:
-        "Android application for leading virtual mobile operator in Poland",
-      logo: MobileVikingsLogo,
-      link: {
-        label: "mobilevikings.pl",
-        href: "https://mobilevikings.pl/",
-      },
-    },
-    {
-      title: "Howdy",
-      techStack: ["Lead Android Developer", "Android", "Kotlin"],
-      description:
-        "Howdy is a place for you to join communities you care about",
-      logo: Howdy,
-      link: {
-        label: "play.google.com",
-        href: "https://howdy.co/",
-      },
-    },
-    {
-      title: "Tastycloud",
-      techStack: ["Lead Android Developer", "Android", "Kotlin"],
-      description:
-        "Android application for managing and displaying restaurant menus in kiosk mode",
-      logo: TastyCloudLogo,
-      link: {
-        label: "tastycloud.fr",
-        href: "https://www.tastycloud.fr/",
-      },
-    },
-    {
-      title: "Ambit",
-      techStack: ["Lead Android Developer", "Android", "Kotlin"],
-      description:
-        "Android application that helps with sharing your contact details",
-      logo: AmbitLogo,
-    },
-    {
-      title: "Bim",
-      techStack: ["Lead Android Developer", "Android", "Kotlin"],
-      description:
-        "Android application that helps with booking a table in a restaurants",
-      logo: BimLogo,
-    },
-    {
-      title: "Canal Digital GO",
-      techStack: ["Lead Android Developer", "Android", "Kotlin"],
-      description:
-        "Video streaming mobile application for Canal Digital subscribers",
-      logo: CDGOLogo,
     },
   ],
 } as const;
